7a3c7e1684cff93f4aad8107a11f095b1647d040
Leo Franchi <lfranchi@kde.org>
2012-08-01 19:18:39 -0400

    Add a QToolbarTabWidget implementation

--- gallery.h
+++ gallery.h
@@ -3,12 +3,22 @@
 
 #include <QWidget>
 
+class QToolbarTabDialog;
+
 class Gallery : public QWidget
 {
     Q_OBJECT
 
 public:
     explicit Gallery(QWidget *parent = 0);
+
+public slots:
+    void showTabToolbarWidget();
+    void tabToolbarWidgetAccepted();
+    void tabToolbarWidgetRejected();
+
+private:
+    QToolbarTabDialog* m_toolbarTabDialog;
 };
 
 #endif // WIDGET_H
